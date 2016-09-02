b9fb82a872f671fd086d1f0415b0862f216a571e
rickywo <rickywo@gmail.com>
2016-09-02 22:45:25 +1000

    migration file modified: Create team_member table

--- db/migrate/20160827133018_create_team_members.rb
+++ db/migrate/20160827133018_create_team_members.rb
@@ -1,4 +1,4 @@
-class CreateTeammembers < ActiveRecord::Migration[5.0]
+class CreateTeamMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :team_members do |t|
       t.integer :team_id
