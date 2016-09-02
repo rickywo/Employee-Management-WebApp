0224ed41b0036af45b850b738279b449dfbab6fb
rickywo <rickywo@gmail.com>
2016-09-02 22:58:36 +1000

    migration file modified: Create team_member table

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
   has_many :projects
   has_many :team_members
-  has_many :employees, :through => :teammembers
+  has_many :employees, :through => :team_members
 end
